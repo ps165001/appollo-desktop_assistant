f5c52e9ed9b4de8139afdb99c8d1699bdd09fc1a
ps165001 <76485072+ps165001@users.noreply.github.com>
2022-03-08 20:41:21 +0530

    Create appollo.py

---
+++ appollo.py
@@ -0,0 +1,164 @@
+import speech_recognition as sr 
+import datetime
+import wikipedia
+import pyttsx3
+import webbrowser
+import random
+import os
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):  
+    engine.say(audio)
+    engine.runAndWait()
+    
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning sir!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon sir!")   
+
+    else:
+        speak("Good Evening sir!")  
+
+    speak(" I am apollo your artificial intelligence. Please tell me how may I help you")    
+    
+
+def takecom():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listning....")
+        audio = r.listen(source)
+    try:
+        print("Recognising.") 
+        query = r.recognize_google(audio,language='en-in')
+        print("pranav: {query}\n")
+    except Exception:               
+        speak("Say that again please...")
+        print("please say that again sir") 
+        return "none"
+    return query
+
+                             
+if __name__ == "__main__":
+    wish()
+    while True:
+        query = takecom().lower()
+
+        if "wikipedia" in query:
+            speak("searching details....Wait")
+            query.replace("wikipedia","")
+            results = wikipedia.summary(query,sentences=2)
+            print(results)
+            speak(results)
+            
+        elif 'open youtube' in query or "open video online" in query:
+            webbrowser.open("www.youtube.com")
+            speak("opening youtube")
+            
+        elif 'open github' in query:
+            webbrowser.open("https://www.github.com")
+            speak("opening github")
+            
+        elif 'open facebook' in query:
+            webbrowser.open("https://www.facebook.com")
+            speak("opening facebook")
+            
+        elif 'open instagram' in query:
+            webbrowser.open("https://www.instagram.com")
+            speak("opening instagram")
+            
+        elif 'open google' in query:
+            webbrowser.open("https://www.google.com")
+            speak("opening google")
+            
+        elif 'open yahoo' in query:
+            webbrowser.open("https://www.yahoo.com")
+            speak("opening yahoo")
+            
+        elif 'open gmail' in query:
+            webbrowser.open("https://mail.google.com")
+            speak("opening google mail") 
+            
+        elif 'open snapdeal' in query:
+            webbrowser.open("https://www.snapdeal.com") 
+            speak("opening snapdeal")  
+             
+        elif 'open amazon' in query or 'shop online' in query:
+            webbrowser.open("https://www.amazon.com")
+            speak("opening amazon")
+            
+        elif 'open flipkart' in query:
+            webbrowser.open("https://www.flipkart.com")
+            speak("opening flipkart")
+            
+        elif 'open ebay' in query:
+            webbrowser.open("https://www.ebay.com")
+            speak("opening ebay")
+            
+        elif 'music from pc' in query or "music" in query:
+            speak("ok sir i am playing music")
+            music_dir = './music'
+            musics = os.listdir(music_dir)
+            os.startfile(os.path.join(music_dir,musics[0]))
+            
+        elif 'video from pc' in query or "video" in query:
+            speak("ok sir i am playing videos")
+            video_dir = './video'
+            videos = os.listdir(music_dir)
+            os.startfile(os.path.join(video_dir,videos[0]))
+            
+        elif 'good bye' in query:
+            speak("good bye")
+            exit()
+            
+        elif "shutdown" in query:
+            speak("shutting down")
+            os.system('shutdown -s')
+            
+        elif "what\'s up" in query or 'how are you' in query:
+            stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy','i am okey ! How are you']
+            ans_q = random.choice(stMsgs)
+            speak(ans_q)  
+            ans_take_from_user_how_are_you = takecom()
+            if 'fine' in ans_take_from_user_how_are_you or 'happy' in ans_take_from_user_how_are_you or 'okey' in ans_take_from_user_how_are_you:
+                speak('okey..')  
+            elif 'not' in ans_take_from_user_how_are_you or 'sad' in ans_take_from_user_how_are_you or 'upset' in ans_take_from_user_how_are_you:
+                speak('oh sorry..')
+                
+            
+        elif "who are you" in query or "about you" in query or "your details" in query:
+            about = "I am apollo an A I based computer program."
+            print(about)
+            speak(about)
+            
+        elif 'what is the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+            
+        elif "you feeling" in query:
+            print("I donot have any feelings")
+            speak("I donot have any feelings")
+            
+        elif query == 'none':
+            continue 
+        elif 'exit' in query or 'abort' in query or 'stop' in query or 'bye' in query or 'quit' in query :
+            ex_exit = 'bye sir. have a nice day'
+            speak(ex_exit)
+            exit()    
+        else:
+            temp = query.replace(' ','+')
+            g_url="https://www.google.com/search?q="    
+            res_g = 'sorry! i cant understand but i search from internet to give your answer ! okay'
+            print(res_g)
+            speak(res_g)
+            webbrowser.open(g_url+temp)                                
+
+            
+            
